2f10738990f55e6552b5d01a590d40dd627fc76d
Andrew Smith <andrew.smith@webmatters.io>
2020-02-02 11:27:29 -0500

    Adds services matrix copy and new favicon.

--- src/components/ServicesMatrix.vue
+++ src/components/ServicesMatrix.vue
@@ -8,7 +8,7 @@
         <h3 class="md:mt-4 text-2xl">Websites</h3>
         <div
           class="px-4"
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo quia ab fugit. Amet optio qui, obcaecati id corrupti nobis vero eum veniam fugiat deleniti.</div>
+        >Your website needs to grab visitors' attention and guide them toward becoming a customer. Strategic design, personality, user flow, SEO, analytics, functionality, speed, reliability. These are must-have requirements for your site.</div>
       </div>
       <div class="md:w-1/3 flex flex-col items-center justify-center md:mr-8 mt-12">
         <div>
@@ -17,16 +17,16 @@
         <h3 class="md:mt-4 text-2xl">Apps</h3>
         <div
           class="px-4"
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo quia ab fugit. Amet optio qui, obcaecati id corrupti nobis vero eum veniam fugiat deleniti.</div>
+        >Do you have an idea for an app that meets a market need? It must have great UX, support mobile devices, and be fast, reliable, & scalable. Leveraging modern frameworks helps achieve these objectives and get to market quickly.</div>
       </div>
       <div class="md:w-1/3 flex flex-col items-center justify-center mt-12">
         <div>
           <image-edit-icon :size="48" />
         </div>
-        <h3 class="text-2xl">Design</h3>
+        <h3 class="md:mt-4 text-2xl">Design</h3>
         <div
           class="px-4"
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo quia ab fugit. Amet optio qui, obcaecati id corrupti nobis vero eum veniam fugiat deleniti.</div>
+        >Your brand is your company's identity. It helps determine whether someone will do business with you. Your selection of colors, typography, logo, business cards, letterhead, and other web and print marketing assets creates your brand.</div>
       </div>
     </div>
     <div class="md:flex text-center md:mt-20">
@@ -34,28 +34,28 @@
         <div>
           <cash-multiple-icon :size="48" />
         </div>
-        <h3 class="text-2xl">Marketing</h3>
+        <h3 class="md:mt-4 text-2xl">Marketing</h3>
         <div
           class="px-4"
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo quia ab fugit. Amet optio qui, obcaecati id corrupti nobis vero eum veniam fugiat deleniti.</div>
+        >The web provides many effective and measurable options to get users to your site: email, Google Ads, Facebook, and many others. The important thing is to determine what provides the best return for your audience.</div>
       </div>
       <div class="md:w-1/3 flex flex-col items-center justify-center md:mr-8 mt-12">
         <div>
           <strategy-icon :size="48" />
         </div>
-        <h3 class="text-2xl">Strategy</h3>
+        <h3 class="md:mt-4 text-2xl">Strategy</h3>
         <div
           class="px-4"
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo quia ab fugit. Amet optio qui, obcaecati id corrupti nobis vero eum veniam fugiat deleniti.</div>
+        >Strategy is not abstract. It's practical, utilizing well-tested processes offered by frameworks like Pragmatic. Without a winning strategy based on an understanding of buyers, markets, and competitors, nothing else matters.</div>
       </div>
       <div class="md:w-1/3 flex flex-col items-center justify-center mt-12">
         <div>
           <chart-bar-icon :size="48" />
         </div>
-        <h3 class="text-2xl">Analytics</h3>
+        <h3 class="md:mt-4 text-2xl">Analytics</h3>
         <div
           class="px-4"
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo quia ab fugit. Amet optio qui, obcaecati id corrupti nobis vero eum veniam fugiat deleniti.</div>
+        >What are you doing with the data you're collecting? Valuable trends and insights can be mined using advanced data science and AI. Reporting, dashboards, and visualizations tell the story of your data to help manage your business.</div>
       </div>
     </div>
   </div>
